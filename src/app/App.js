db6ad18858538666bf3ae1a7ebaa6b11005cec67
David Delgadillo <daviddel.731@gmail.com>
2021-05-03 08:31:11 -0700

    fixed errors

--- src/app/App.js
+++ src/app/App.js
@@ -3,45 +3,67 @@ import './App.css';
 import React from 'react';
 import request from 'superagent';
 import Header from './Header';
+import PokeList from '../pokemon/PokeList';
+import Search from './Search';
 
 const POKEDEX_API = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 class App extends Component {
   state = {
-    pokedex: []
+    pokemon: []
   }
 
-  componentDidMount() {
-    this.fetchPokedex();
+  // handleSearch = ({ search }) => {
+  //   this.setState(
+  //     { search: search, page: 1 },
+  //     () => this.fetchPokemon()
+  //   );
+  // }
 
+  async componentDidMount() {
+    const response = await request.get(POKEDEX_API);
+    this.setState({ pokemon: response.body.results });
   }
 
-  async fetchPokedex() {
-    
+  // async fetchPokemon() {
+  //   const { search, page } = this.state;
 
-    try {
-      const response = await request.get(POKEDEX_API);
-      this.setState({ 
-        pokedex: response.body
-      });
+  //   this.setState({ loading: true });
 
-    } catch (ERROR) {
-      console.log(ERROR);
-    } finally {
-      this.setState ({
-        pokedex : []
-      });
-    }
-  }
+  //   try {
+  //     const response = await request
+  //       .get(POKEDEX_API)
+  //       .query({ name: search })
+  //       .query({ page: page });
+
+  //     this.setState({ 
+  //       pokedex: response.body
+  //     });
+
+  //   } catch (ERROR) {
+  //     console.log(ERROR);
+  //   } finally {
+  //     this.setState ({
+  //       pokedex : []
+  //     });
+  //   }
+  // }
 
   render() {
+    const { pokemon } = this.state;
+
     return (
       <div className="App">
 
         <Header/>
+        <PokeList pokemon={pokemon}/>
+
+        <section className="search-bar">
+          <Search onSearch={this.handleSearch}/>
+        </section>
 
         <main>
-          List goes here! 
+          
         </main>
       
       </div>
